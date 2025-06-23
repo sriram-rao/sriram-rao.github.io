92cd090a5ae773b7578838840b03823fb687575d
Sriram Rao <sriram.rao04@gmail.com>
2025-06-22 19:38:16 -0700

    Connect links spacing refinements

--- src/routes/connect.svelte
+++ src/routes/connect.svelte
@@ -2,22 +2,26 @@
   import LinkTo from "./linkto.svelte";
 </script>
 
-<div class="flex flex-row place-content-center items-center gap-10">
-  <p class="text-base color-prominent">
-    Connect with me<br />
+<div
+  class="md:flex md:flex-row place-content-center items-center gap-3 md:gap-10"
+>
+  <div class="mb-3">
+    <p class="text-base color-prominent">Connect with me</p>
     <span class="text-sm color-subhead">Are you hiring?</span>
-  </p>
+  </div>
 
-  <LinkTo text="Email" href="mailto:reach@sriramrao.com" style="btn-accent" />
-  <LinkTo
-    text="LinkedIn"
-    href="https://www.linkedin.com/in/sriram-rao"
-    style="btn-info"
-  />
+  <span class="flex place-content-center justify-between md:gap-5">
+    <LinkTo text="Email" href="mailto:reach@sriramrao.com" style="btn-accent" />
+    <LinkTo
+      text="LinkedIn"
+      href="https://www.linkedin.com/in/sriram-rao"
+      style="btn-info"
+    />
 
-  <LinkTo
-    text="GitHub"
-    href="https://www.github.com/sriram-rao/"
-    style="btn-default"
-  />
+    <LinkTo
+      text="GitHub"
+      href="https://www.github.com/sriram-rao/"
+      style="btn-default"
+    />
+  </span>
 </div>
