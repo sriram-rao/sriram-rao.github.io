c6e95bdcc4d8033fe355419853592ccf5b0e825c
Sriram Rao <sriram.rao04@gmail.com>
2025-08-27 02:02:06 -0700

    Fixing top menu links that I forgot the 1st time

--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -6,9 +6,9 @@
   <div class="dropdown md:hidden m-4">
     <div tabindex="0" role="button" class="btn">Menu</div>
     <ul class="menu dropdown-content menu-compact rounded-box bg-base-100">
-      <li><a href="#">Home</a></li>
-      <li><a href="#experience">Experience</a></li>
-      <li><a href="#skills">Skills</a></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/#experience">Experience</a></li>
+      <li><a href="/#skills">Skills</a></li>
       <li>
         <a href="/blog" class="text-success">Blog</a>
       </li>
